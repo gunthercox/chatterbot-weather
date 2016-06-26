1c84414e37815f31910504608e721d1a71ebd2fa
Gunther Cox <gunthercx@gmail.com>
2016-06-26 11:56:25 -0400

    Add an example chat bot

---
+++ examples/weather_example.py
@@ -0,0 +1,14 @@
+from chatterbot import ChatBot
+
+# Create a new chat bot
+chatbot = ChatBot(
+    "Weather Bot",
+    logic_adapters=[
+        chatterbot_weather.weather_adapter.WeatherLogicAdapter
+    ]
+)
+
+# Ask a question about the weather
+response = chatbot.get_response("What is the weather like today?")
+
+print(response)
