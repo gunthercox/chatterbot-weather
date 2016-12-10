01f7c06fe1c85d8d43466964b8d1f076ad31a2e2
Gunther Cox <gunthercx@gmail.com>
2016-12-09 20:19:46 -0500

    Fix package info

--- chatterbot_weather/__init__.py
+++ chatterbot_weather/__init__.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
+"""
+A ChatterBot logic adapter that returns information about the weather.
+"""
+
 from .weather_adapter import WeatherLogicAdapter
 
 __author__ = 'Gunther Cox'
 __email__ = 'gunthercx@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.1'
+__url__ = 'https://github.com/gunthercox/chatterbot-weather'
--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ try:
 except ImportError:
     from distutils.core import setup
 
+# Dynamically retrieve the version information from the chatterbot module
+CHATTERBOT = __import__('chatterbot_weather')
+VERSION = CHATTERBOT.__version__
+AUTHOR = CHATTERBOT.__author__
+AUTHOR_EMAIL = CHATTERBOT.__email__
+URL = CHATTERBOT.__url__
+DESCRIPTION = CHATTERBOT.__doc__
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -17,37 +24,34 @@ with open('HISTORY.rst') as history_file:
 with open('requirements.txt') as history_file:
     requirements = history_file.read()
 
-with open('requirements_dev.txt') as history_file:
-    test_requirements = history_file.read()
+with open('requirements_dev.txt') as dev_file:
+    test_requirements = dev_file.read()
 
 setup(
     name='chatterbot-weather',
-    version='0.1.0',
-    description="A ChatterBot logic adapter that returns information about the weather.",
+    version=VERSION,
+    description=DESCRIPTION,
     long_description=readme + '\n\n' + history,
-    author="Gunther Cox",
-    author_email='gunthercx@gmail.com',
-    url='https://github.com/gunthercox/chatterbot-weather',
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
     packages=[
         'chatterbot_weather',
     ],
-    package_dir={'chatterbot-weather':
-                 'chatterbot-weather'},
+    package_dir={'chatterbot-weather': 'chatterbot-weather'},
     include_package_data=True,
     install_requires=requirements,
-    license="ISCL",
+    license="BSD",
     zip_safe=True,
     keywords='chatterbot-weather',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
+        'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
